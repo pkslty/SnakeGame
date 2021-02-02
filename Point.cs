4da0d18ca2865f7ce52ac685e545c832b0bed4a1
Denis Kuzmin <pkslty@yandex.ru>
2021-02-02 12:37:46 +0300

    Добавлен класс point

---
+++ Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    class Point
+    {
+        //Координаты точки
+        public int x, y;
+        public char symbol;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 5;
+            p1.symbol = '#';
+            p1.Draw();
+
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }

504575f64bacfe317c05d2ea433298a97dcedd2a
Denis Kuzmin <pkslty@yandex.ru>
2021-02-02 13:40:28 +0300

    Добавлены конструкторы в класс Point

--- Point.cs
+++ Point.cs
@@ -6,5 +6,29 @@ namespace SnakeGame
 {
     class Point
     {
+        //Координаты точки
+        public int x, y;
+        public char symbol;
+
+        //Конструкторы
+        public Point()
+        {
+
+        }
+        
+        public Point(int x, int y, char symbol)
+        {
+            this.x = x;
+            this.y = y;
+            this.symbol = symbol;
+        }
+
+        //Функция рисования
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 5;
-            p1.symbol = '#';
+            Point p1 = new Point(1, 5, '#');
+            Point p2 = new Point();
+            p2.x = 3;
+            p2.y = 6;
+            p2.symbol = '*';
             p1.Draw();
+            p2.Draw();
 
             //Console.WriteLine("Hello World!");
             Console.ReadLine();
